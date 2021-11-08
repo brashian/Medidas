4e4070e4a7cc43813d059fe2d3a6efa5e8ffb528
brashian <bsandovala12@gmail.com>
2021-11-08 17:07:41 -0300

    Fin

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 
-
-
-
 //console.log(option);
 //console.log(parseInt(numero));
 let resultado = 0;
+
 const option = document.getElementById('option');
 option.addEventListener('change',seleccionar);
+
 const select = document.getElementById('select')
 select.addEventListener('change',seleccionar);
 
 const number = document.getElementById('numero');
 number.addEventListener('input',seleccionar);
 
+
+
    function seleccionar(){
+       
        let option = document.getElementById('option').value;
        let numero = document.getElementById('numero').value;
        let convertir = document.getElementById('select').value;
@@ -21,46 +23,43 @@ number.addEventListener('input',seleccionar);
         
 
         switch(option){
-            case "cm":{
+            case "cm":
                 switch(convertir){
-                    case "Cm":{resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mm":{resultado= numero * 10; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mts":{resultado= numero / 100; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Km":{resultado= numero / 100000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    default:{console.log("seleccion medida valida");break;}
+                    case "Mm":resultado= numero * 10; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Mts":resultado= numero / 100; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Km":resultado= numero / 100000; document.getElementById("cambiar").innerHTML = resultado;break
+                    default:resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break
                 };
-            break;}
+            break;
 
-            case "mm":{
+            case "mm":
                 switch(convertir){
-                    case "Cm":{resultado= numero /10; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mm":{resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mts":{resultado= numero / 1000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Km":{resultado= numero / 1000000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    default:{console.log("seleccion medida valida");break;}
+                    case "Cm":resultado= numero /10; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Mts":resultado= numero / 1000; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Km":resultado= numero / 1000000; document.getElementById("cambiar").innerHTML = resultado;break
+                    default:resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break
                 };
-            break;}
+            break;
             
-            case "mts":{
+            case "mts":
                 switch(convertir){
-                    case "Cm":{resultado= numero*100; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mm":{resultado= numero *1000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mts":{resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Km":{resultado= numero / 1000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    default:{console.log("seleccion medida valida");break;}
+                    case "Cm":resultado= numero*100; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Mm":resultado= numero *1000; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Km":resultado= numero / 1000; document.getElementById("cambiar").innerHTML = resultado;break
+                    default:resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break
                 };
-            break;}
+            break;
 
-            case "km":{
+            case "km":
                 switch(convertir){
-                    case "Cm":{resultado= numero*100000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mm":{resultado= numero * 1000000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Mts":{resultado= numero * 1000; document.getElementById("cambiar").innerHTML = resultado;break}
-                    case "Km":{resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break}
-                    default:{console.log("seleccion medida valida");break;}
+                    case "Cm":resultado= numero*100000; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Mm":resultado= numero * 1000000; document.getElementById("cambiar").innerHTML = resultado;break
+                    case "Mts":resultado= numero * 1000; document.getElementById("cambiar").innerHTML = resultado;break
+                    default:resultado= numero; document.getElementById("cambiar").innerHTML = resultado;break
                 };
-            break;}
-            default:{console.log("seleccion medida valida");break;}
+            break;
+
+            default:console.log("seleccion medida valida");break;
         }
     }
 
